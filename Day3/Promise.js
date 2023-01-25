ac636a7badf750a7b9eca7ca4ddb41261481dd36
CHITRESH Kumar <chitreshkumar.bsr@gmail.com>
2023-01-25 18:08:01 +0530

    up

--- Day3/Promise.js
+++ Day3/Promise.js
@@ -28,7 +28,7 @@
 
 
 // let myPromise = new Promise(function(myResolve, myReject) {
-//     setTimeout(function() { myResolve("I love You !!"); }, 3000);
+//     setTimeout(function() { myResolve("HII Hello!"); }, 5000);
 //   });
 
 
