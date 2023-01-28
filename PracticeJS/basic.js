0f8b8ee9a4df85751aa89713254d2ca910dc917e
CHITRESH Kumar <chitreshkumar.bsr@gmail.com>
2023-01-28 08:28:48 +0530

    new work

---
+++ PracticeJS/basic.js
@@ -0,0 +1,30 @@
+// var z,a = 50;
+// console.time("Test")  //start time
+// console.log(['a','d','c'])   //array
+// console.table(['a','d','c'])   // show array in table form
+// console.error("Somthing went wrong")   // produce error
+// console.warn("Somthing ")   // show warning
+// console.warn("went ")      // show warning
+// console.warn("wrong ")     // show warning
+// console.clear("wrong ")       // clear console
+// console.timeEnd("Test")      //  end time in milisec
+// Itteration condition
+// console.log(z = (a != 50)? "hello you are write":"Oops you are wrong");
+// var a = confirm("What are you looking for!")
+// alert(a);
+// var a = prompt("What is your name");
+// alert(a);
+
+// < button onclick="hello()"></button>
+// ondblclick  //doubleclick
+// oncontextmenu  //right click
+// onmouseenter  //mouse hover
+// onmouseout  //mouse out
+// onmousedown  //mouse down
+// onmouseup  //mouse up
+// onkeypress  //key press
+// onkeyup  //key up
+// onload  //load
+// onunload  //unload
+// onresize  //resize
+// onscroll  //scroll
---
+++ PracticeJS/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+
+// console.log(['a','d','c'])   //array
+// console.table(['a','d','c'])   // show array in table form
+// console.error("Somthing went wrong")   // produce error
+// console.warn("Somthing ")   // show warning
+// console.warn("went ")      // show warning
+// console.warn("wrong ")     // show warning
+// console.clear("wrong ")       // clear console
+// console.timeEnd("Test")      //  end time in milisec
